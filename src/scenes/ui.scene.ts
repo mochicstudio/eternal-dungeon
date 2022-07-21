cd38f63015bf33fa52889e96cc08e13f6627e317
andrdc <andrsruiz@gmail.com>
2022-07-21 12:02:51 -0500

    chapter5 Starting UI scene

--- src/scenes/ui.scene.ts
+++ src/scenes/ui.scene.ts
@@ -1,9 +1,22 @@
 import Phaser from 'phaser';
+import { turnManager } from '../classes/turn-manager';
 
 class UI extends Phaser.Scene {
+  private created: boolean;
+
   constructor() {
     super('UI');
+
+    this.created = false;
   }
+
+  create() {
+    this.scene.get('EternalDungeon').events.on('renderUI', () => this.render());
+  }
+
+  update() { }
+
+  render() { }
 }
 
 const ui = new UI();
